ec2e1bad49dd0f4ef90ae1d14b7d72a45e7bbd1c
Om Deshmukh <od66@cornell.edu>
2018-05-30 12:11:11 -0700

    Updating lab 0 to have ping in the timeout class

--- modules/pingpong/src/framework/pingpong/Timeouts.java
+++ modules/pingpong/src/framework/pingpong/Timeouts.java
@@ -1,11 +1,14 @@
 package framework.pingpong;
 
 import framework.Timeout;
+import framework.pingpong.PingApplication.Ping;
 import lombok.Data;
 
 @Data
 final class PingTimeout implements Timeout {
     private static final int PING_TIMEOUT_MILLIS = 10;
+
+    private final Ping ping;
 
     @Override
     public int timeoutLengthMillis() {

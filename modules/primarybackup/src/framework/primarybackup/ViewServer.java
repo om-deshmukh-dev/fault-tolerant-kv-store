304b55fcce6062861ea199df7ec2d952014e11a9
Om Deshmukh <od66@cornell.edu>
2025-09-20 20:37:25 -0400

    fix: optimized view server to install more aggressively

--- modules/primarybackup/src/framework/primarybackup/ViewServer.java
+++ modules/primarybackup/src/framework/primarybackup/ViewServer.java
@@ -48,9 +48,11 @@ class ViewServer extends Node {
   private void handlePing(Ping m, Address sender) {
     // don't care what their view number is, or whether the ping is duplicated
     nextAliveServers.add(sender);
-
     canMoveOnToNewView |= (sender.equals(view.primary()) && m.viewNum() == view.viewNum());
 
+    if (canMoveOnToNewView) {
+      tryPrematureInstallIdleServer();
+    }
     send(new ViewReply(view), sender);
   }
 
@@ -66,8 +68,8 @@ class ViewServer extends Node {
     currAliveServers = nextAliveServers;
     nextAliveServers = new HashSet<>();
 
-    if (canMoveOnToNewView) {
-      attemptViewTransition();
+    if (canMoveOnToNewView && (isDead(view.primary()) || isDead(view.backup()))) {
+      handlePrimaryBackupFailure();
     }
     set(t, PING_CHECK_MILLIS);
   }
@@ -76,58 +78,84 @@ class ViewServer extends Node {
    *  Utils
    * ---------------------------------------------------------------------------------------------*/
 
-  // handle primary/backup failure, or install new server as primary/backup in case of no failure
-  private void attemptViewTransition() {
-    if (isDead(view.primary()) && isDead(view.backup())) {
-      // cry
-    } else if (isDead(view.primary())) {
-      changeView(view.backup(), null);
-    } else if (isDead(view.backup())) {
-      changeView(view.primary(), null);
-    } else {
-      tryInstallNewServer();
-    }
-  }
+  /**
+   * Attempt to move on to new view if there is an idle server that has pinged but is not yet alive,
+   * and a primary/backup needs to be installed. Is premature since the idle server is not yet alive.
+   */
+  private void tryPrematureInstallIdleServer() {
+    assert canMoveOnToNewView;
 
-  private void tryInstallNewServer() {
     // only install a new server if an alive idle server exists
-    if (!existsAliveIdleServer()) {
-      return;
-    }
+    if (!existsIdleServer(nextAliveServers)) { return; }
 
     if (view.primary() == null && view.backup() == null) {
-      changeView(selectAliveIdleServer(), null);
-    }
-    else if (view.backup() == null) {
-      changeView(view.primary(), selectAliveIdleServer());
+      changeView(selectIdleServer(nextAliveServers), null);
+    } else if (view.backup() == null) {
+      changeView(view.primary(), selectIdleServer(nextAliveServers));
     }
   }
 
-  // selects an alive idle server, assuming one exists
-  private Address selectAliveIdleServer() {
-    assert existsAliveIdleServer();
+  /**
+   * Handle primary/backup failure:
+   *  - primary failure: transition to new view with a new idle server as backup (or null if none found)
+   *  - backup failure: transition to new view with backup as primary and idle server as new backup (or null if none found)
+   */
+  private void handlePrimaryBackupFailure() {
+    assert canMoveOnToNewView;
 
-    for (Address server : currAliveServers) {
-      if (!server.equals(view.primary()) && !server.equals(view.backup())) {
+    // primary failure where backup can replace it
+    if (isDead(view.primary()) && view.backup() != null && !isDead(view.backup())) {
+      changeView(view.backup(), selectIdleServer(currAliveServers));
+    }
+
+    // backup failure
+    if (isDead(view.backup())) {
+      assert view.primary() != null;
+      changeView(view.primary(), selectIdleServer(currAliveServers));
+    }
+  }
+
+
+  /**
+   * Selects an idle server from the set of servers passed in, or returns null if none exist
+   */
+  private Address selectIdleServer(HashSet<Address> servers) {
+    for (Address server : servers) {
+      if (isIdle(server)) {
         return server;
       }
     }
-    return null; // should not get here
+    return null;
   }
 
-  private boolean existsAliveIdleServer() {
-    return currAliveServers.stream().anyMatch((server) ->
-        !server.equals(view.primary()) && !server.equals(view.backup()));
+  private boolean existsIdleServer(HashSet<Address> servers) {
+    return servers.stream().anyMatch(this::isIdle);
   }
 
+  /**
+   * Update the current view with an incremented view number, and new primary/backup.
+   */
   private void changeView(Address primary, Address backup) {
+    assert canMoveOnToNewView;
+
     int viewNumNext = (view.viewNum() == STARTUP_VIEWNUM) ? INITIAL_VIEWNUM : view.viewNum() + 1;
     view = new View(viewNumNext, primary, backup);
     canMoveOnToNewView = false; // must wait until primary acknowledges view
   }
 
-  // if an actual server is not in the set of currently alive servers
+  /**
+   * Determines whether the server is not in the set of currently alive servers. Returns false
+   * if null is passed in (only valid addresses can be defined as dead).
+   */
   private boolean isDead(Address server) {
     return server != null && !currAliveServers.contains(server);
+  }
+
+  /**
+   * Determines whether the server is idle, meaning that it that doesn't match the primary or
+   * backup in the current view. Returns false if null is passed in (only valid addresses can be idle).
+   */
+  private boolean isIdle(Address server) {
+    return server != null && !server.equals(view.primary()) && !server.equals(view.backup());
   }
 }
